cc88414a70fde65e005b12a35db8b69f64bdc498
adminov <batishnazik@gmail.com>
2021-09-02 14:11:09 +0600

    'hardTask03'

--- js.js
+++ js.js
@@ -1,20 +1,29 @@
 'use strict';
 
-const num = 266219;
+const lang = 'en';
 
-const numbig = (numStr) => {
-    let res = 1;
-    for (let i = 0; i < numStr.length; i++) {
-        res = res * numStr[i];
-    }
-    return res;
+//через ив
+if (lang === 'ru'){
+    console.log('пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс');
+} else {
+    console.log('mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn');
+}
+
+switch (lang) {
+    case "en":
+        console.log('mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn');
+        break;
+    case "ru":
+        console.log('пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс');
+    default:
+}
+
+const lan = {
+    'ru': ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
+    'en': ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn']
 };
 
-const resNum = numbig(num.toString());
-console.log(resNum);
+console.log(lan[lang]);
 
-
-const step = resNum ** 3;
-console.log(step);
-
-console.log(step.toString().substr(0, 2));
+const namePerson = 'Alex';
+console.log((namePerson === 'Alex') ? 'CEO' : (namePerson === 'Maxim') ? 'teacher' : 'student');
