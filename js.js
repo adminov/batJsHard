3dc0e8ad98a00af6eeb37d0cd2f09d81113c076f
adminov <batishnazik@gmail.com>
2021-09-11 20:22:10 +0600

    'hardTask17.2'

--- js.js
+++ js.js
@@ -1,30 +1,8 @@
 'use strict';
 
-const cityArr = {
-    rus: ['Москва', 'Санк-Петербург', 'Новосибирск', 'Екатеринбург', 'Нижний Новгород', 'Казань', 'Челябинск'],
-    uk: ['Киев', 'Харьков', 'Одесса', 'Днепр', 'Донецк', 'Запорожье', 'Львов'],
-    bel: ['Минск', 'Гомель', 'Могилёв', 'Витебск', 'Гродно', 'Брест'],
-    jap: ['Токио', 'Киото', 'Осака', 'Иокогама']
-};
+function getResult(x, y) {
+    let result = (Math.pow(x, y) + '').split('').map(item => +item).reduce((accum, item) => accum + item);
+    return result;
+}
 
-const country = document.getElementById('country');
-const result = document.querySelector('.result');
-const city = document.getElementById('city');
-// console.log(cityArr['jap']);
-
-const selectCountry = (event) => {
-    city.style.display = 'inline-block';
-    city.innerHTML = '';
-    cityArr[event.target.value].forEach((item) => {
-        const option = document.createElement("option");
-        option.innerHTML = item;
-        option.value = event.target.options[event.target.options.selectedIndex].text;
-        city.append(option);
-    });
-};
-
-country.addEventListener("change", selectCountry);
-city.addEventListener("change", (event) => {
-    result.innerHTML = event.target.value + ', ' +
-        event.target.options[event.target.options.selectedIndex].text;;
-});
+console.log(getResult(3, 10));
