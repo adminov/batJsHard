7be2ccec213fd93a5cd0e735d5f30e998372d12c
adminov <batishnazik@gmail.com>
2021-09-11 13:02:48 +0600

    'hardTask16'

--- js.js
+++ js.js
@@ -1,70 +1,30 @@
-document.addEventListener('DOMContentLoaded', function () {
-    'use strict';
-    function DomElement(selector, height, width, bg, fontSize, absolute) {
-        this.selector = selector;
-        this.width = width;
-        this.height = height;
-        this.bg = bg;
-        this.fontSize = fontSize;
-        this.absolute = absolute;
+'use strict';
+
+const myLesson = [
+    {lesson: 1, type: 'basic', points: 2},
+    {lesson: 2, type: 'additional', points: 4},
+    {lesson: 3, type: 'basic', points: 6},
+    {lesson: 4, type: 'additional', points: 3},
+    {lesson: 5, type: 'basic', points: 4},
+    {lesson: 6, type: 'basic', points: 2},
+    {lesson: 7, type: 'additional', points: 2},
+    {lesson: 8, type: 'basic', points: 6},
+    {lesson: 9, type: 'basic', points: 4},
+    {lesson: 10, type: 'basic', points: 6},
+    {lesson: 11, type: 'additional', points: 5},
+    {lesson: 12, type: 'basic', points: 2},
+    {lesson: 13, type: 'additional', points: 2},
+    {lesson: 14, type: 'basic', points: 4},
+    {lesson: 15, type: 'additional', points: 1},
+    {lesson: 16, type: 'additional', points: 7},
+];
+
+for (let i = myLesson.length - 1;  i >= 0; i-- ){
+    const type = myLesson[i].type;
+    if (type === 'additional'){
+        myLesson.splice(myLesson.indexOf(myLesson[i]), 1);
+    } else if (type === 'basic') {
+        myLesson[i].points = myLesson[i].points / 2;
     }
-
-    DomElement.prototype.start = function(){
-        let div;
-        if (this.selector.substr(0, 1) === '.') {
-            div = document.createElement('div');
-            div.className = this.selector.replace('.', '');
-        } else if (this.selector.substr(0, 1) === '#') {
-            div = document.createElement('p');
-            div.id = this.selector.replace('#', '');
-        }
-
-        div.style.cssText = `
-        width: ${this.width}px;
-        height: ${this.height}px;
-        background: ${this.bg};
-        font-size: ${this.fontSize}px;
-        position: ${this.absolute}`;
-        document.body.append(div);
-        div.textContent = 'hello everyone';
-    };
-
-    // let text = document.querySelector('.bar');
-    // text.textContent  = 'hello everyone';
-
-    let newDomElem = new DomElement('.bar', 100, 100, 'red', 10, 'absolute');
-
-    newDomElem.start();
-
-    //Написать обработчик события для keydown, который будет принимать callback-функцию.
-    // Данная функция будет отлавливать нажатие на стрелки клавиатуры. В зависимости от нажатой
-    // кнопки(Вверх - стрелка вверх, Влево - стрелка влево, Вправо - стрелка вправо, Вниз - стрелка
-    // вниз) наш квадрат будет перемещаться на 10 пикселей.
-
-
-    let top = 0,
-        left = 0;
-    document.addEventListener('keydown', function () {
-        let block = document.querySelector('div');
-        switch (event.keyCode) {
-            case 39: //вправо
-                left = left + 10;
-                block.style.left = left + 'px';
-                break;
-            case 37: //влево
-                left = left - 10;
-                block.style.left = left + 'px';
-                break;
-            case 38: //вверх
-                top = top - 10;
-                block.style.top = top + 'px';
-                break;
-            case 40: //вниз
-                top = top + 10;
-                block.style.top = top + 'px';
-                break;
-        }
-    })
-
-
-});
+}
+console.log(myLesson);
